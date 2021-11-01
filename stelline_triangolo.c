aa70e12eb0e3526ff4dd8a078c2de6acca5bf9e3
rosacarota <rosa.carota.01@gmail.com>
2021-11-01 17:45:31 +0100

    nuovo file:             stelline_triangolo.c

---
+++ stelline_triangolo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	int i, j = 1, n;
+	
+	printf("Inserisci la lunghezza della piramide: ");
+	scanf("%d", &n);
+	
+	while(n != 0){
+		for(i = n - 1; i != 0; i--){	
+			printf(" ");		
+		}	
+		for(i = 0; i < j; i++){
+			printf("*");
+		}
+
+		printf("\n");
+		n--;
+		j += 2;
+	}
+
+	return 0;
+}
