a23963d229387e408e7b55f59523091e488e8cf4
RosaCarota <rosa.carota.01@gmail.com>
2022-03-23 18:23:30 +0100

    nuovo file:             a.txt

---
+++ PSD/03_22/Laboratorio/Attivita_4/distanza_massima/implementazione_file/test_suite/file.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "punto.h"
+
+FILE* finput_array(char *file_name);
---
+++ PSD/03_22/Laboratorio/Attivita_4/distanza_massima/implementazione_file/test_suite/punto.h
@@ -0,0 +1,9 @@
+
+typedef struct miopunto *punto;
+
+punto creapunto(float x, float y); 
+float ascissa(punto p);
+float ordinata(punto p);
+float distanza(punto p1, punto p2);
+void stampa(punto p);
+
