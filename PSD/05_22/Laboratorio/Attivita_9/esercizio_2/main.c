d76200ad551693557679724c71e3d13b3415f2d1
RosaCarota <rosa.carota.01@gmail.com>
2022-05-13 09:32:53 +0200

    x#	modificato:             main.c

--- PSD/05_22/Laboratorio/Attivita_9/esercizio_2/main.c
+++ PSD/05_22/Laboratorio/Attivita_9/esercizio_2/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int quadrato(int n) {
-    if(n == 1) return 1;
+    if (n < 0) 
+        n *= -1;
 
+    if(n == 1) return 1;
+        
     return (2 * n - 1) + quadrato(n - 1);
 }
 
 int main(void) {
-    int n = 10;
+    int n = -5;
     int quad; 
 
-    quad = quadrato(10);
+    quad = quadrato(n);
 
     printf("Il quadrato di %d e' %d", n , quad);
 
